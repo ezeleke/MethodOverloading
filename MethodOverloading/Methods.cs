c5ec463dacecaff0df26e0b10cd405c6968f4e06
ezeleke <abefikru@gmail.com>
2022-08-15 12:48:19 -0700

    methodsOverloading commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;   
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = 0;
+            string answer = "";
+
+            if (isTrue)
+            {
+                sum = a + b;
+
+                if (sum == 1)
+                {
+                    return $" {sum} dollar.";
+                }
+                else
+                {
+                    return $" {sum} dollars.";
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Methods.Add(27, 1, true));
+        }
+    }
+}
